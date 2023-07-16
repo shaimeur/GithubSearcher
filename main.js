738e465180b6b442fa8063d36c8f66dbfca3b961
shaimeur <haimeursaad@gmail.com>
2023-07-17 00:27:34 +0100

    creating the searchGithub function that retrieave the username from the input and then passe it to the fetchUser function to fetch data from the gitHubb API and display it in object

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-let btn = document.querySelector('button')
+let input = document.querySelector('#searchInput')
 
-const searchGithub = () =>{
-    console.log("the serach bar have been hited!!")
+const searchGithub = (event) =>{
+        let userName = '' ;
+           userName = event.target.value ;
+            console.log(userName)
+            // return userName ;
+            fetchUser(userName)
 }
 
-btn.addEventListener('click',searchGithub)
+const fetchUser = async (userName) =>{
+    let response = await fetch(`https://api.github.com/users/${userName}`) ;
+    if(response.ok) {
+        let result = await response.json()
+        console.log(result)
+    }else{
+        console.log('Error user not found')
+    }
+
+}
+
+
+input.addEventListener('change',searchGithub)
